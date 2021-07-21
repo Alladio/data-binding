46873836367a05a8db4a79ebb4b13e00515e67cf
Alladio Bonesso <alladio.bonesso@hotmail.com>
2021-07-21 04:54:17 -0300

    AULA 13 - REVER

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+  // styles:[
+  //   `
+  //   .highlight{
+  //     background-color: yellow;
+  //     font-weight: bold;
+  // }
+  // `
+  //]
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = "https://pajeh.com";
+  cursoAngular: boolean = true;
+  urlImagem: string = "http://lorempixel.com/400/200/nature/";
+
+  valorAtual:  string ="";
+
+  valorSalvo: string ="" ;
+
+  isMouseOver: boolean= false;
+
+  // nome:string="abcd";
+
+  // pessoa: any = {
+  //   nome: 'Alladio',
+  //   idade: 32
+  // }
+
+  nomeDoCurso:string="Angular";
+
+  botaoClicado(){
+    alert("Clicou!");
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: string){
+
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver =!this.isMouseOver;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/meu-form/meu-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-form',
+  templateUrl: './meu-form.component.html',
+  styleUrls: ['./meu-form.component.css']
+})
+export class MeuFormComponent implements OnInit {
+  
+
+  nome:string="abcd";
+
+  pessoa: any = {
+    nome: 'Alladio',
+    idade: 32
+  }
+  botaoClicado(){
+    alert("Clicou!");
+  }
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
